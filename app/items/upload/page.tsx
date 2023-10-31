208d3d46e63b486fd268f6995c59e6703fd6ebe1
Boyeon Ihn <boyeonihn@gmail.com>
2023-10-31 20:26:37 +0900

    style: apply styling to upload.tsx

--- app/items/upload/page.tsx
+++ app/items/upload/page.tsx
@@ -20,25 +20,40 @@ const Upload = () => {
           <input type="file" className="hidden" />
         </label>
       </section>
-      <section>
-        <label htmlFor="price">Price</label>
-        <div>
-          <div>
-            <span>$</span>
+      <section className="my-5">
+        <label
+          htmlFor="price"
+          className="mb-1 block text-sm font-medium text-gray-700"
+        >
+          Price
+        </label>
+        <div className="rounded-md shadow-sm relative flex items-center">
+          <div className="absolute left-0 pl-3 pointer-events-none flex items-center justify-center">
+            <span className="text-gray-500 text-sm">$</span>
           </div>
-          <input id="price" type="text" placeholder="0.00" />
-          <div>
-            <span>USD</span>
+          <input
+            id="price"
+            type="text"
+            className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+            placeholder="0.00"
+          />
+          <div className="absolute right-0 pointer-events-none pr-3 flex items-center ">
+            <span className="text-gray-500">USD</span>
           </div>
         </div>
       </section>
       <section>
-        <label>Description</label>
-        <div>
-          <textarea rows={4} />
-        </div>
+        <label className="mb-1 block text-sm font-medium text-gray-700">
+          Description
+        </label>
+        <textarea
+          className="mt-1 shadow-sm w-full focus:ring-1 focus:ring-orange-500 rounded-md focus:border-orange-500 border-gray-300"
+          rows={4}
+        />
       </section>
-      <button>Upload product</button>
+      <button className="mt-4 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
+        Upload product
+      </button>
     </main>
   );
 };
