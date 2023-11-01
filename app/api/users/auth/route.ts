8e9a1f8fb63a9425e8c1646e30ed380f2ceae9bd
Boyeon Ihn <boyeonihn@gmail.com>
2023-11-02 05:36:55 +0900

    feat: create user upsert function post login

--- app/api/users/auth/route.ts
+++ app/api/users/auth/route.ts
@@ -1,6 +1,20 @@
 import { NextResponse } from 'next/server';
+import client from '@/_libs/server/client';
 
 export const POST = async (req: Request) => {
   const res = await req.json();
+  const { phone, email } = res;
+
+  let user;
+  user = await client.user.upsert({
+    where: {
+      phone: +phone,
+    },
+    create: {
+      phone: +phone,
+      name: 'Anonymous',
+    },
+    update: {},
+  });
   return NextResponse.json({ res, message: 'ok' });
 };
