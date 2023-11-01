bdae2fb85df2827ae8b6747bc8858637e04a152a
Boyeon Ihn <boyeonihn@gmail.com>
2023-11-02 06:34:34 +0900

    feat: create token auth

--- app/api/users/auth/route.ts
+++ app/api/users/auth/route.ts
@@ -5,7 +5,9 @@ export const POST = async (req: Request) => {
   const res = await req.json();
   const { phone, email } = res;
 
-  const payload = phone ? { phone: +phone } : { email };
+  const payload = phone ? { phone } : { email };
+
+  console.log('paylod', payload);
   const user = await client.user.upsert({
     where: {
       ...payload,
@@ -16,5 +18,25 @@ export const POST = async (req: Request) => {
     },
     update: {},
   });
+
+  const token = await client.token.create({
+    data: {
+      payload: '1233334',
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: 'Anonymous',
+            ...payload,
+          },
+        },
+      },
+    },
+  });
+
+  console.log('token', token);
+
   return NextResponse.json({ res, message: 'ok' });
 };
