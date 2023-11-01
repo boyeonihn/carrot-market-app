62b652cdaa62275e5dc9685aeec3b3e2d68f6490
Boyeon Ihn <boyeonihn@gmail.com>
2023-11-01 19:39:59 +0900

    chore: fix alias path for cls function

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { cls } from './_libs/client/utils';
+import { cls } from '@/_libs/client/utils';
 
 const inter = Inter({ subsets: ['latin'] });
 
