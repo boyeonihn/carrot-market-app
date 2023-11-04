ee275209486bf0ade2391ff8a5b88ceb3c19cedf
Boyeon Ihn <boyeonihn@gmail.com>
2023-11-04 15:05:16 +0900

    feat: implement iron-session ren-crhis version to build session

--- app/_libs/server/session.ts
+++ app/_libs/server/session.ts
@@ -1,17 +1,58 @@
-import { unsealData } from 'iron-session/edge';
+import {
+  IronSessionOptions,
+  getIronSession,
+  IronSessionData,
+  getServerActionIronSession,
+} from 'iron-session';
+
 import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
 
-const session = async () => {
-  const cookieStore = cookies();
-  const encryptedSession = cookieStore.get(process.env.COOKIE_AUTH!)?.value;
-
-  const sessionState = encryptedSession
-    ? await unsealData(encryptedSession, {
-        password: process.env.COOKIE_PW!,
-      })
-    : null;
-
-  return sessionState;
+// session object options
+export const sessionOptions: IronSessionOptions = {
+  password: process.env.COOKIE_PW!,
+  cookieName: process.env.COOKIE_AUTH!,
+  cookieOptions: {
+    secure: process.env.NODE_ENV === 'production',
+  },
 };
 
-export default session;
+declare module 'iron-session' {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
+
+// API route handler that uses getIronSession and returns response that needs to be called from client-side
+const getSession = async (req: NextRequest, res: NextResponse) => {
+  const session = getIronSession<IronSessionData>(req, res, sessionOptions);
+  return session;
+};
+
+// uses cookies() function from next/headers to set cookies so that Iron session can be used in nextJS server actions and react server components
+const getServerActionSession = async () => {
+  const session = getServerActionIronSession<IronSessionData>(
+    sessionOptions,
+    cookies()
+  );
+  return session;
+};
+
+// const session = async () => {
+//   const cookieStore = cookies();
+//   const encryptedSession = cookieStore.get(process.env.COOKIE_AUTH!)?.value;
+
+//   const sessionState = encryptedSession
+//     ? await unsealData(encryptedSession, {
+//         password: process.env.COOKIE_PW!,
+//       })
+//     : null;
+
+//   return sessionState;
+// };
+
+// export default session;
+
+export { getSession, getServerActionSession };
